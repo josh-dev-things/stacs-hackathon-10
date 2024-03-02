3fd5e75fb9bdc6452ecd1491fadebb6c03e6f15d
st275 <st275@st-andrews.ac.uk>
2024-03-02 21:06:07 +0000

    Idk progress

--- rendering.js
+++ rendering.js
@@ -1,17 +1,37 @@
 const url = "http://trenco.cs.st-andrews.ac.uk:24480/data/atms/brands/santander-uk-plc?";
 const min_lat = 49
-const max_lat = 59
-const min_long = -7
-const max_long = 2
+const max_lat = 61
+const min_lon = -8
+const max_lon = 2
+const incr = .1
 
 window.addEventListener("load", () => {
     populateGrid();
 })
 
 function populateGrid() {
-    for (let long=min_long; long<max_long; long += .1) {
-        for (let lat = max_lat; lat>min_lat; lat -= .1){
-            let q = `${url}latmin=${lat}&latmax=${lat+.1}&lonmin=${long-.1}&lonmax=${long}`
+    for (let lat = max_lat; lat>min_lat; lat -= incr){
+        for (let lon = min_lon; lon<max_lon; lon += incr) {
+            let q = `${url}latmin=${lat-incr}&latmax=${lat}&lonmin=${lon}&lonmax=${lon+incr}`
+            fetch (q)
+                .then (r => r.json())
+                .then(response => {
+                    let size = response.length
+                    if (size > 0) {
+                        appendToGrid(true, lon, lat);
+                    } else { //if no atm in range
+                        appendToGrid(false, lon, lat);
+                    }
+                })
+                .catch(error => {
+                    console.log(`Error: ${error}`)
+                })
+        }
+    }
+    
+    /* for (let long=min_long; long<max_long; long += incr) {
+        for (let lat = max_lat; lat>min_lat; lat -= incr){
+            let q = `${url}latmin=${lat}&latmax=${lat+incr}&lonmin=${long-incr}&lonmax=${long}`
             fetch (q)
                 .then (r => r.json())
                 .then(response => {
@@ -26,7 +46,7 @@ function populateGrid() {
                     console.log(`Error: ${error}`)
                 })
         }
-    }
+    } */
 }
 
 function appendToGrid(bool, long, lat) {
@@ -34,58 +54,39 @@ function appendToGrid(bool, long, lat) {
     if (bool) {
         newDiv.id = `${long},${lat}`;
         newDiv.classList.add("grid-item");
+        /* newDiv.addEventListener("onclick", () => {
+            displayData(long,lat)
+        }); */
     } else {
         newDiv.classList.add("filler");
     }
     document.getElementById("map").appendChild(newDiv);
 }
 
-function getAtms() {
-    let wheelchair = document.getElementById("waInput").value;
-    let town = document.getElementById("townInput").value;
-    let hour = document.getElementById("hourInput").value;
-
-    let myURL = url;
-    
-    if (wheelchair) myURL += `wa=${wheelchair}&`;
-    if (pc) myURL += `postcode=${pc}&`;
-    if (hour) myURL += `aoi=${hour}&`;
-
-    fetch(myURL)
-        .then(response => {
-            console.log(response.status);
-            return response.json();
-        })
-        .then(data => {
-            let parentElement = document.getElementById('atmInfo'); 
-        
-            data.forEach(atm => {
-                let atmElement = document.createElement('div');
-        
-                let servicesElement = document.createElement('p');
-                servicesElement.textContent = `ATM Services: ${atm.ATMServices}`;
-                atmElement.appendChild(servicesElement);
-        
-                let amountElement = document.createElement('p');
-                amountElement.textContent = `Minimum Possible Amount: ${atm.MinimumPossibleAmount}`;
-                atmElement.appendChild(amountElement);
-        
-                let branchElement = document.createElement('p');
-                branchElement.textContent = `Branch ID: ${atm.Branch.Identification}`;
-                atmElement.appendChild(branchElement);
-        
-                let accessibilityElement = document.createElement('p');
-                accessibilityElement.textContent = `Accessibility: ${atm.Accessibility}`;
-                atmElement.appendChild(accessibilityElement);
-
-                let townElement = document.createElement('p');
-                townElement.textContent = `Town: ${atm.Location.PostalAddress.TownName}`;
-                atmElement.appendChild(townElement);
-        
-                parentElement.appendChild(atmElement);
-            });
+function displayData(long,lat) {
+    let q = `${url}latmin=${lat}&latmax=${lat+incr}&lonmin=${long-incr}&lonmax=${long}`
+    fetch(q)
+        .then (r => r.json())
+        .then (response => {
+            response.forEach(atm => {
+                let atmInfo = document.createElement('p');
+                let text = `Town: ${atm.Location.PostalAddress.TownName}\n`
+                text += `Accessibility Features: ${atm.Accessibility}\n`
+                text += `24 hours: ${atm.Access24HoursIndicator ? "Yes" : "No"}\n`
+                atmInfo.appendChild(document.createTextNode(text))
+                nodes.append(atmInfo)
+                //parentNode.appendChild(atmInfo);
+            })
         })
         .catch(error => {
-            console.log("Error: " + error);
-        });
+            console.log(`Error: ${error}`)
+        })
+    let parentNode = document.getElementById("info");
+    while (parentNode.hasChildNodes()){
+        parentNode.removeChild(parentNode.firstChild);
+    }
+    let nodes = []
+    for (const element in nodes) {
+        parentNode.appendChild(element);
+    }
 }
