d58da1c14626326851810a969b4feae0ca3d8536
st275 <st275@st-andrews.ac.uk>
2024-03-02 19:36:16 +0000

    Mapping only places where ATM

--- rendering.js
+++ rendering.js
@@ -1,58 +1,57 @@
-/* import data from "./ncr-data-set/atms.json";
-const atms_json_path = "./ncr-data-set/atms.json";
-const atms = JSON.parse(data)
-console.log(atms); */
-const min_long = 49
-const max_long = 59
-const min_lat = -7
-const max_lat = 2
+const url = "http://trenco.cs.st-andrews.ac.uk:24480/data/atms/brands/santander-uk-plc?";
+const min_lat = 49
+const max_lat = 59
+const min_long = -7
+const max_long = 2
 
 window.addEventListener("load", () => {
     populateGrid();
 })
 
 function populateGrid() {
-    const grid = document.getElementById("map");
-    for (let long=max_long; long>min_long; long -= .1) {
-        for (let lat = min_lat; lat<max_lat; lat += .1){
-            newDiv = document.createElement("div");
-            newDiv.id = `${long},${lat}`;
-            newDiv.classList.add("grid-item");
-            
-            // if there is atm in rnage
-            /* if (atmListed(long,lat)) {
-                newDiv.id = `${long},${lat}`;
-                newDiv.classList.add("grid-item");
-            } else { //if no atm in range
-                newDiv.classList.add("filler");
-            } */
-            grid.appendChild(newDiv);
+    for (let long=min_long; long<max_long; long += .1) {
+        for (let lat = max_lat; lat>min_lat; lat -= .1){
+            let q = `${url}latmin=${lat}&latmax=${lat+.1}&lonmin=${long-.1}&lonmax=${long}`
+            fetch (q)
+                .then (r => r.json())
+                .then(response => {
+                    let size = response.length
+                    if (size > 0) {
+                        appendToGrid(true, long, lat);
+                    } else { //if no atm in range
+                        appendToGrid(false, long, lat);
+                    }
+                })
+                .catch(error => {
+                    console.log(`Error: ${error}`)
+                })
         }
     }
 }
 
-/* function atmListed(long,lat) {
-    list = atms.data.Brand.ATM;
-    for (const atm in list) {
-        let coords = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates; 
-        if (atm.Longitude === long && atm.Latitude === lat) {
-            return true
-        }
+function appendToGrid(bool, long, lat) {
+    newDiv = document.createElement("div");
+    if (bool) {
+        newDiv.id = `${long},${lat}`;
+        newDiv.classList.add("grid-item");
+    } else {
+        newDiv.classList.add("filler");
     }
-    return false;
-} */
+    document.getElementById("map").appendChild(newDiv);
+}
+
 function getAtms() {
     let wheelchair = document.getElementById("waInput").value;
     let pc = document.getElementById("pcInput").value;
     let hour = document.getElementById("hourInput").value;
 
-    let url = `http://trenco.cs.st-andrews.ac.uk:24480/data/atms/brands/santander-uk-plc/?`;
+    let myURL = url;
+    
+    if (wheelchair) myURL += `wa=${wheelchair}&`;
+    if (pc) myURL += `postcode=${pc}&`;
+    if (hour) myURL += `aoi=${hour}&`;
 
-    if (wheelchair) url += `wa=${wheelchair}&`;
-    if (pc) url += `postcode=${pc}&`;
-    if (hour) url += `aoi=${hour}&`;
-
-    fetch(url)
+    fetch(myURL)
         .then(response => {
             console.log(response.status);
             return response.json();
