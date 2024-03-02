7a283f5bc26c64ed94d7879fe3b1a3ff3f501327
josh-dev-things <ethankessler.dev@gmail.com>
2024-03-02 19:58:26 +0000

    Basic Branches

--- server/server.js
+++ server/server.js
@@ -36,13 +36,23 @@ function CreateServer(port = 24480)
     });
 
     app.get("/data/atms", function(req, res){
-
         try {
             let result = filterATMSByQuery(req);
             res.status(200).json(result);
-            console.log(`${Time()}  |   GET /data/atms -> [200] JSON object returned`);
+            console.log(`${Time()}  |   GET /data/atms -> [200] JSON object returned (${result.length})`);
         } catch(error) {
             console.error(`${Time()}    |   ERROR (GET /data/atms) : "${error}"`);
+            res.status(500).json({});
+        }
+    });
+
+    app.get("/data/branches", function(req, res){
+        try{
+            let result = filterBranchesByQuery(req);
+            res.status(200).json(result);
+            console.log(`${Time()}  |   GET /data/branches -> [200] JSON object returned (${branches.length})`);
+        } catch (error) {
+            console.error(`${Time()}    |   ERROR (GET /data/branches) : "${error}"`);
             res.status(500).json({});
         }
     });
@@ -54,16 +64,63 @@ function CreateServer(port = 24480)
             {
                 let result = filterATMSByQuery(req);
                 res.status(200).json(result);
-                console.log(`${Time()}  |   GET /data/atms/:brand -> [200] JSON object returned`);
+                console.log(`${Time()}  |   GET /data/atms/${req.params.brand} -> [200] JSON object returned (${result.length})`);
             } else {
                 res.status(200).json({});
-                console.log(`${Time()}  |   GET /data/atms/:brand -> [200] Empty Response`);
+                console.log(`${Time()}  |   GET /data/atms/${req.params.brand}-> [200] Empty Response`);
             }
         } catch(error) {
-            console.error(`${Time()}    |   ERROR (GET /data/atms/:brand) : "${error}"`);
+            console.error(`${Time()}    |   ERROR (GET /data/atms/${req.params.brand}) : "${error}"`);
             res.status(500).json({});
         }
     });
+
+    app.get("/data/branches/brands/:brand", function(req, res){
+        try {
+            let brand_branches = branches.data[0].Brand.filter(jso => isBrand(req.params.brand, jso));
+            if(brand_branches.length > 0)
+            {
+                let result = filterBranchesByQuery(req);
+                res.status(200).json(result);
+                console.log(`${Time()}  |   GET /data/branches/brands/${req.params.brand} -> [200] JSON object returned (${result.length})`);
+            } else {
+                res.status(200).json({});
+                console.log(`${Time()}  |   GET /data/branches/brands/${req.params.brand} -> [200] Empty Response`);
+            }
+        } catch(error) {
+            console.error(`${Time()}    |   ERROR (GET /data/branches/brands/${req.params.brand}) : "${error}"`);
+            res.status(500).json({});
+        }
+    });
+
+    function filterBranchesByQuery(req)
+    {
+        let id = req.query.id;
+
+        function satisfiesQuery(branch) {
+
+            if(id) {
+                if(branch.Identification.toLowerCase() !== id.toLowerCase())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        let branch_objects = [];
+
+        branches.data[0].Brand.forEach(b => {
+            b.Branch.forEach(branch => {
+                if(satisfiesQuery(branch)) {
+                    branch_objects.push(branch);
+                }
+            });
+        });
+
+        return branch_objects;
+    }
 
     function filterATMSByQuery(req)
     {
@@ -82,6 +139,7 @@ function CreateServer(port = 24480)
         let lon_min = Number(req.query.lonmin);
         let lon_max = Number(req.query.lonmax);
 
+        let town = req.query.town;
 
         // Open 24 hours?
         let aoi = req.query.aoi; // always open
@@ -168,6 +226,13 @@ function CreateServer(port = 24480)
                 let coords = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates;
                 if(Number(coords.Latitude) > lat_max || Number(coords.Latitude) < lat_min)
                 {
+                    return false;
+                }
+            }
+
+            if(town)
+            {
+                if(town.toLowerCase() !== atm.Location.PostalAddress.TownName.toLowerCase()){
                     return false;
                 }
             }
