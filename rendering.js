8480e9c5c3e24e431f31bf6b00ad41e6006bf6fc
st275 <st275@st-andrews.ac.uk>
2024-03-02 16:38:07 +0000

    HTML dev

--- rendering.js
+++ rendering.js
@@ -1,3 +1,7 @@
+/* import data from "./ncr-data-set/atms.json";
+const atms_json_path = "./ncr-data-set/atms.json";
+const atms = JSON.parse(data)
+console.log(atms); */
 const min_long = 49
 const max_long = 59
 const min_lat = -7
@@ -12,11 +16,28 @@ function populateGrid() {
     for (let long=max_long; long>min_long; long -= .1) {
         for (let lat = min_lat; lat<max_lat; lat += .1){
             newDiv = document.createElement("div");
-            // if there is atm in rnage
             newDiv.id = `${long},${lat}`;
             newDiv.classList.add("grid-item");
+            
+            // if there is atm in rnage
+            /* if (atmListed(long,lat)) {
+                newDiv.id = `${long},${lat}`;
+                newDiv.classList.add("grid-item");
+            } else { //if no atm in range
+                newDiv.classList.add("filler");
+            } */
             grid.appendChild(newDiv);
-            //if no atm in range
         }
     }
 }
+
+/* function atmListed(long,lat) {
+    list = atms.data.Brand.ATM;
+    for (const atm in list) {
+        let coords = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates; 
+        if (atm.Longitude === long && atm.Latitude === lat) {
+            return true
+        }
+    }
+    return false;
+} */
